db07661e2f2c8c42c84cb01a1d0f348a2b6cf739
root <mngenge@gmail.com>
2022-10-18 10:10:46 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet -helps print all alpabet letters in lower case
+ */
+void print_alphabet(void)
+{
+char lt;
+for (lt = 'a'; lt <= 'z'; lt++)
+{
+_putchar(lt);
+}
+putchar('\n');
+}
