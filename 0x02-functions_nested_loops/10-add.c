e4a52df6c3944d10bbbc00492783d8668cb816a9
root <mngenge@gmail.com>
2022-10-18 16:58:22 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#incude "main.h"
+
+/**
+ * add - add two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the value of addition
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
