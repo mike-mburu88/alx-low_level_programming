b2cc29f814c8208aa54eddc1593ac3779d5a283f
root <mngenge@gmail.com>
2022-10-18 08:33:48 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - A program that prints putchar at every line
+*Return: Always 0
+*/
+
+int main(void)
+{
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This is an exercise on nested loop functions
