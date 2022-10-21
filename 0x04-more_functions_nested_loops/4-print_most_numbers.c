b399dde3e6871ee685dcd3c962a41968a25276f2
root <mngenge@gmail.com>
2022-10-21 08:39:00 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ *print_most_numbers - function to print numbers
+ *Return value: 0
+ */
+void print_most_numbers(void)
+{
+char x;
+for (x = 0; x <= 9; x++)
+{
+if (x != 2 && x != 4)
+_putchar(x);
+_putchar('\n');
+}
+}
