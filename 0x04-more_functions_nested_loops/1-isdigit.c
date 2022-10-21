3cd1ae0c29199b00748d749390c788321973fe84
root <mngenge@gmail.com>
2022-10-21 06:58:19 -0700

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,12 @@
 
 /**
  *_isdigit - check if number is 0 to 9
- *@c: char
+ *@c: char to check
  *Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
-int c;
 if (c >= '0' && c <= '9')
 return (1);
 else
