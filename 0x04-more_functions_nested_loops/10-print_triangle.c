989c789eb2092bf3dbd8fc5f4e0645c7625478f1
root <mngenge@gmail.com>
2022-10-21 15:06:02 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int row, hash, space;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (space = size - row; space >= 1; space--)
+{
+_putchar(' ');
+}
+for (hash = 1; hash <= row; hash++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
