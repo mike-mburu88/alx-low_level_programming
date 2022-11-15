71413b496d2136585bc490892d3890fa5798f99e
root <mngenge@gmail.com>
2022-11-15 07:05:16 -0800

    task 0

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - a function that  prints the filename it was compiled from
+ *Return: Always 0
+ */
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,8 @@
+The following folder contains preprocessor directive exercises
+start with task 0
+
+
+
+
+
+q
