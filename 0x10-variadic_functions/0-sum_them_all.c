8ce94da6ea0f8014e2e05cbc3feee1fcffedeb47
root <mngenge@gmail.com>
2022-11-18 04:45:53 -0800

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,14 @@ unsigned int x;
 int sum = 0;
 va_list ptr;
 if (n == 0)
+{
 return (0);
+}
 va_start(ptr, n);
 for (x = 0; x < n; x++)
+{
 sum += va_arg(ptr, int);
 va_end(ptr);
+}
 return (sum);
 }
