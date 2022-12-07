c516b77a6d283ac5a9acc3c8b3d3d4775b691cae
root <mngenge@gmail.com>
2022-12-07 07:23:10 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *create_file - a function that creates a file
+ *@filename: the name of the file to be created
+ *@text_content: argument for the textual content
+ *Return: 1 on success, -1 if failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fnum;
+int xletters;
+int rwrtext;
+if (!filename)
+return (-1);
+fnum = open(filename, O_WRONLY | O_APPEND);
+if (fnum == -1)
+return (-1);
+if (text_content)
+{
+for (xletters = 0; text_content[xletters]; xletters++)
+rwrtext = write(fnum, text_content, xletters);
+if (rwrtext == -1)
+return (-1);
+}
+close(fnum);
+return (1);
+}
