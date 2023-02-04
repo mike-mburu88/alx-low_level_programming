5f0f7f829dfc8a252bd137b2802fa6074d11f7fa
root <mngenge@gmail.com>
2023-02-03 21:30:20 -0800

    task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,11 +7,11 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-unsigned long int idx;
+unsigned long int index;
 hash_node_t *tmp;
 if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0)
 return (NULL);
-idx = key_index((const unsigned char *)key, ht->size);
+index = key_index((const unsigned char *)key, ht->size);
 tmp = ht->array[index];
 while (tmp != NULL)
 {
