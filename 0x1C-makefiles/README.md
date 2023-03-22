932df8824519f02da7700cb02a8e356f60af3de1
root <mngenge@gmail.com>
2023-03-22 04:39:49 -0700

    the first makefile for school.c

---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,3 @@
+The following folder contains
+projects that are related to 
+makefiles in the C language
---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,9 @@
+#ifndef M_H_
+#define M_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_school(void);
+
+#endif
